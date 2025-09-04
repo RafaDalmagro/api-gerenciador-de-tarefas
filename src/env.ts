d183fdf44e24e7b34f76b2941c1fcaa5f2414516
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-09-03 21:42:25 -0300

    removendo a validação do .env das variáveis que não serão usadas no deploy.

--- src/env.ts
+++ src/env.ts
@@ -4,9 +4,9 @@ const envSchema = z.object({
     JWT_SECRET: z.string(),
     DATABASE_URL: z.string(),
     PORT: z.coerce.number().default(3000),
-    POSTGRES_USER: z.string(),
-    POSTGRES_PASSWORD: z.string(),
-    POSTGRES_DB: z.string(),
+    // POSTGRES_USER: z.string(),
+    // POSTGRES_PASSWORD: z.string(),
+    // POSTGRES_DB: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
